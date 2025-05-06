01b284f4c93648fccc4eeefbde2ea8836400dda6
Alfredo <alvaradoferral@gmail.com>
2025-05-05 20:41:04 -0600

    URL actualizado

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # configuration.py
 
-URL_SERVICE = "https://cnt-41ccd746-7424-4a31-bb03-847a9e876e34.containerhub.tripleten-services.com"  # La URL del servidor de Urban Grocers
+URL_SERVICE = "https://cnt-08118d7a-05c1-4391-8b26-0e6cb14de259.containerhub.tripleten-services.com"  # La URL del servidor de Urban Grocers
 CREATE_USER_PATH = "/api/v1/kits"  # Endpoint para la creación de kits
 DOCS_PATH = "/docs/"  # Documentación de la API
